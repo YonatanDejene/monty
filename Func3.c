4c52b716bc274dd0b118c3caff9eb5ff7a321545
yonatandejene <yonatan.dejene@gmail.com>
2023-08-24 17:05:02 +0300

    func3 and 4

--- Func3.c
+++ Func3.c
@@ -4,55 +4,6 @@ void monty_pstr(stack_t **stack, unsigned int line_number);
 void monty_nop(stack_t **stack, unsigned int line_number);
 void monty_pchar(stack_t **stack, unsigned int line_number);
 
-/**
- * monty_pstr - Prints the string in a stack.
- * @stack: A pointer.
- * @line_number: The current working line number.
- */
-void monty_pstr(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp = (*stack)->next;
-
-	while (temp && temp->n != 0 && (temp->n > 0 && temp->n <= 127))
-	{
-		printf("%c", temp->n);
-		temp = temp->next;
-	}
-
-	printf("\n");
-
-	(void)line_number;
-}
-
-/**
- * monty_pchar - Prints the character in the top value.
- * @stack: A pointer.
- * @line_number: The current working line number.
- */
-void monty_pchar(stack_t **stack, unsigned int line_number)
-{
-	if ((*stack)->next->n < 0 || (*stack)->next->n > 127)
-	{
-		set_op_tok_error(pchar_error(line_number,
-					     "value out of range"));
-		return;
-	}
-
-	if ((*stack)->next == NULL)
-	{
-		set_op_tok_error(pchar_error(line_number, "stack empty"));
-		return;
-	}
-
-	printf("%c\n", (*stack)->next->n);
-}
-
-/**
- * monty_nop - Does nothing for Monty opcode nop.
- * @stack: A pointer.
- * @line_number: The current working line number.
- */
-
 void monty_nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
