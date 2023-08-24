320406c13577e820659fbcdb3eb5e3e2064cebe2
yonatandejene <yonatan.dejene@gmail.com>
2023-08-24 17:10:05 +0300

    modified

--- Func3.c
+++ Func3.c
@@ -1,9 +1,12 @@
 #include "monty.h"
 
-void monty_pstr(stack_t **stack, unsigned int line_number);
 void monty_nop(stack_t **stack, unsigned int line_number);
-void monty_pchar(stack_t **stack, unsigned int line_number);
 
+/**
+ * monty_nop - Does absolutely nothing.
+ * @stack: A pointer.
+ * @line_number: The current working line number.
+ */
 void monty_nop(stack_t **stack, unsigned int line_number)
 {
 	(void)stack;
